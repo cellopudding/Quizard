d9f02a00927273851afd54bc33263831b0d813d1
Jeremiah Dorvil <jeremiahdorvil@outlook.com>
2023-03-15 21:26:10 -0400

    updated category routes

--- controllers/api/scoreRoutes.js
+++ controllers/api/scoreRoutes.js
@@ -1,5 +1,23 @@
-const router = require('express').Router();
-const sequelize = require('../../config/connection');
-const { Score } = require('../../models');
-const withAuth = require('../../utils/auth');
+// const router = require('express').Router();
+// const sequelize = require('../../config/connection');
+// const { Score } = require('../../models');
+// const withAuth = require('../../utils/auth');
 
+// router.post('/score', async, withAuth (req, res) => {
+//     try {
+//         const newScore = await Score.create(req.body);
+//         console.log(newScore);
+        
+//         req.session.save(() => {
+//             req.session.user_id = userData.id;
+//             req.session.logged_in = true;
+//         })
+//         res.status(200).json(newScore);
+//     } catch (err) {
+//         console.log(err);
+//         res.status(400).json(err);
+//     }
+// });
+
+
+// module.exports = router;
