8cefad9c1b9a2780d1035c7b3728651c9b3da734
laurszalk <laurenszalk@gmail.com>
2023-03-19 11:26:21 -0400

    fixed login and logout

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -9,42 +9,23 @@ const question = document.querySelector(".question-container");
 const questionCard = document.querySelector(".question-card");
 
 function startQuiz(e) {
-
   // console.log(e.target)
   // console.log(this);
-  // // hide start screen
-  // var categoriesScreen = document.querySelector(".categories-container");
-  // categoriesScreen.setAttribute("class", "hide");
-
-  // will need to update this to some kind of changeColor function
-  // questionCard.classList.add(this.getAttribute("id"));
-
-  // // un-hide questions section
-  // question.removeAttribute("class", "hide");
 
   getApi(this.getAttribute("id"));
- 
-  console.log(this.getAttribute("id"))
- }
- categories.forEach(function (i) {
-  i.addEventListener('click', startQuiz) })
-// for (const category of categories) {
-//   category.addEventLister("click", startQuiz);
-  // 
-  
-// }
 
-// var newQuestion = document.getElementById(params);
+  console.log(this.getAttribute("id"));
+}
+categories.forEach(function (i) {
+  i.addEventListener("click", startQuiz);
+});
+
+// var newQuestion = document.getElementById("science");
 
 const getApi = async (id) => {
-  console.log(id);
   const response = await fetch(`/api/quiz/${id}`, {
     method: "GET",
-   
     headers: { "Content-Type": "application/json" },
   });
-
-}
-//  category.addEventListener("click", getApi);
-
-
+};
+// newQuestion.addEventListener("click", getApi);
